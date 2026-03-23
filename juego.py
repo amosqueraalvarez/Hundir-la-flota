cf4b7bd8627e973117623fe7afe045ca30f2ab09
Adrian <amosqueraalvarez@danielcastelao.org>
2026-03-23 12:29:20 +0100

    inicio

---
+++ juego.py
@@ -0,0 +1,28 @@
+from tablero import Tablero
+
+
+class Juego:
+    def __init__(self):
+        self.tablero = Tablero()
+        self.lanzar_ataque(1, 1)
+        self.lanzar_ataque(1, 2)
+        self.lanzar_ataque(1, 3)
+        self.lanzar_ataque(1, 4)
+        self.lanzar_ataque(1, 5)
+
+    def lanzar_ataque(self, x, y):
+        print(f"Ataque a {x},{y}")
+        resultado = self.tablero.comprobar_impacto(x, y)
+        self.mostrar_resultado(resultado)
+
+    def mostrar_resultado(self, resultado):
+        if resultado == 0:
+            print("Agua")
+        elif resultado == 1:
+            print("Tocado")
+        else:
+            print("Hundido")
+
+
+if __name__ == "__main__":
+    Juego()
